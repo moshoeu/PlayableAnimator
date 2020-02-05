ed4815afe547ed1e9ddb0f01b241b65d90c21903
ZhangYuhao <zuu1996@foxmail.com>
2020-02-06 00:41:47 +0800

    状态管理

---
+++ Assets/Scripts/PlayableAmimatorDriver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace PlayableAnimator
+{
+    public class PlayableAmimatorDriver : PlayableBehaviour
+    {
+        private PlayableGraph m_Graph;
+        private Playable m_Playable;
+        private PlayableStateController m_StateController;
+
+        public PlayableAmimatorDriver(PlayableGraph graph)
+        {
+            m_Graph = graph;
+            m_StateController = new PlayableStateController(graph);
+        }
+
+        public override void OnPlayableCreate(Playable playable)
+        {
+            m_Playable = playable;
+        }
+
+        public override void PrepareFrame(Playable playable, FrameData info)
+        {
+            //m_StateController.
+        }
+    }
+}
